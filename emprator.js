39e2921a1d2bbed651ba621904ec4c4d95f0acdd
pasha <69049072+baapasha@users.noreply.github.com>
2021-03-16 22:08:27 +0300

    Update emprator.js

--- emprator.js
+++ emprator.js
@@ -73,7 +73,7 @@ client.on("message", saewan => {
       saewan.author.send(`> **تـۆش ریکلام بۆ ئـەم سێرڤـەرە بکە **  
 `); /// لینکە سیرڤەرە خۆت
 
-      client.channels.get("741776396518686720").send(////ئایدی ژوری تێکست
+      client.channels.get("").send(////ئایدی ژوری تێکست
  
         `> Nerdra la layan <@${saewan.author.id}> 
 ${saewan.content}`
